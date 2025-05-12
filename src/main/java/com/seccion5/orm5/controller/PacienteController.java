695391e85ecc268bd540baf901f5c282e9f12bd2
Chiispyn <dem.monsalves@duocuc.cl>
2025-05-12 12:53:53 -0400

    buscar por id

--- src/main/java/com/seccion5/orm5/controller/PacienteController.java
+++ src/main/java/com/seccion5/orm5/controller/PacienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seccion5.orm5.model.Paciente;
 import com.seccion5.orm5.service.PacienteService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
@@ -32,6 +39,23 @@ public class PacienteController {
         return new ResponseEntity<>(pacientes,HttpStatus.OK);
     };
     
+    @PostMapping
+    public ResponseEntity<Paciente> postPaciente(@RequestBody Paciente paciente) {
+        Paciente buscado=pacienteService.findById(paciente.getId());
+        if(buscado==null){
+        return new ResponseEntity<>(pacienteService.save(paciente),HttpStatus.CREATED);}
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> getPacienteById(@PathVariable int id) {
+        Paciente buscado=pacienteService.findById(id);
+        
+         if(buscado!=null){
+            return new ResponseEntity<>(buscado,HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     
     
     
--- src/main/java/com/seccion5/orm5/repository/PacienteRepository.java
+++ src/main/java/com/seccion5/orm5/repository/PacienteRepository.java
@@ -11,4 +11,9 @@ import com.seccion5.orm5.model.Paciente;
 public interface PacienteRepository extends JpaRepository<Paciente, Integer>{
     List<Paciente> findAll();
     
+
+    @SuppressWarnings("unchecked")
+    Paciente save(Paciente paciente);
+        
+    Paciente findById(int id);
 }
